6021a3c920af4d3fb87582078e7c9c140ca097c1
Hela Konečná <lazarova.hela@gmail.com>
2021-03-17 20:32:18 +0100

    ukolc.1

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// tady je místo pro náš program
+//let odstavec = document.querySelector('p');
+
+function ztucni(odstavec){
+    odstavec.target.style.fontWeight = "bold";
+}
+
+function odtucni(odstavec){
+    odstavec.target.style.fontWeight = "normal";
+}
+
+function zcervena(element){
+    let toRed = document.querySelector(element) ;
+    toRed.classList.toggle("barva");
+}
+
+
+function zvetsi(element){
+    let currSize = document.querySelector(element).style.fontSize ;
+
+    } 
+    
+
+//@param {string} elementA
+
+function prehraj(elementA) {
+   let audio = document.querySelector(elementA) ;
+   audio.play();
+}
+
+function stopni(elementB) {
+    let audio = document.querySelector(elementB) ;
+    audio.pause();
+ }
